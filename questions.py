5004612055d344daf65da86b9b7b778985c9e93b
josephsurrey <josephsurrey2018@gmail.com>
2023-05-05 20:52:23 +1200

    Create questions.py

---
+++ questions.py
@@ -0,0 +1,34 @@
+""" Written by Joseph Surrey 05/05/2023
+"""
+
+MAORI_NUMBERS = [["What is the Māori word for one?", "tahi"],
+                 ["What is the Māori word for two?", "rua"],
+                 ["What is the Māori word for three?", "toru"],
+                 ["What is the Māori word for four?", "whā"],
+                 ["What is the Māori word for five?", "rima"],
+                 ["What is the Māori word for six?", "ono"],
+                 ["What is the Māori word for seven?", "whitu"],
+                 ["What is the Māori word for eight?", "waru"],
+                 ["What is the Māori word for nine?", "iwa"],
+                 ["What is the Māori word for ten?", "tekau"]]
+MAORI_MONTHS = [["What is the Māori word for January?", "Kohitātea"],
+                ["What is the Māori word for February?", "Hui-tanguru"],
+                ["What is the Māori word for March?", "Poutū-te-rangi"],
+                ["What is the Māori word for April?", "Paenga-whāwhā"],
+                ["What is the Māori word for May?", "Haratua"],
+                ["What is the Māori word for June?", "Pipiri"],
+                ["What is the Māori word for July?", "Hōngongoi"],
+                ["What is the Māori word for August?", "Here-turi-kōkā"],
+                ["What is the Māori word for September?", "Mahuru"],
+                ["What is the Māori word for October?", "Whiringa-ā-nuku"],
+                ["What is the Māori word for November?", "Whiringa-ā-rangi"],
+                ["What is the Māori word for December?", "Hakihea"]]
+MAORI_DAYS = [["What is the Māori word for Monday?", "Rāhina"],
+              ["What is the Māori word for Tuesday?", "Rātū"],
+              ["What is the Māori word for Wednesday?", "Rāapa"],
+              ["What is the Māori word for Thursday?", "Rāpare"],
+              ["What is the Māori word for Friday?", "Rāmere"],
+              ["What is the Māori word for Saturday?", "Rāhoroi"],
+              ["What is the Māori word for Sunday?", "Rātapu"]]
+
+
