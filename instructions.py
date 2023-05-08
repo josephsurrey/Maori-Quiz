69bb38719a08b9e4c89bb44b1c8b0f442786689f
josephsurrey <josephsurrey2018@gmail.com>
2023-05-08 17:23:07 +1200

    Create instructions.py

---
+++ instructions.py
@@ -0,0 +1,10 @@
+""" Written by Joseph Surrey 08/05/2023
+"""
+
+from setup import *
+from quiz_selection import quiz_selection
+
+
+def instructions():
+    print(INSTRUCTIONS)
+    quiz_selection()
