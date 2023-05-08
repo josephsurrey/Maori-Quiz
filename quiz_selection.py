e908819f27584a5c54abe9e5fafbb2d7e304e5c1
josephsurrey <josephsurrey2018@gmail.com>
2023-05-08 17:33:32 +1200

    Create quiz_selection.py

---
+++ quiz_selection.py
@@ -0,0 +1,20 @@
+""" Written by Joseph Surrey 08/05/2023
+"""
+from ask_question import ask_question
+from get_valid_input import get_valid_input
+from questions import *
+
+
+def quiz_selection():
+    quiz = get_valid_input("Please select a quiz:\n"
+                           "1) Maori numbers 1-10\n"
+                           "2) Maori months of the year\n"
+                           "3) Maori days of the week\n"
+                           "PLease enter a number between 1 and 3 to select quiz: ", int, [1, 2, 3])
+    if quiz == 1:
+        questions = MAORI_NUMBERS
+    elif quiz == 2:
+        questions = MAORI_MONTHS
+    else:
+        questions = MAORI_DAYS
+    ask_question(questions)
