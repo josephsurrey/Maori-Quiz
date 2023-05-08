607bd52a55c0aaa1e68816add62b95f1621d065e
josephsurrey <josephsurrey2018@gmail.com>
2023-05-08 17:39:08 +1200

    added comments to quiz selection

--- quiz_selection.py
+++ quiz_selection.py
@@ -6,15 +6,18 @@ from questions import *
 
 
 def quiz_selection():
+    # ask user what quiz they would like to do
     quiz = get_valid_input("Please select a quiz:\n"
                            "1) Maori numbers 1-10\n"
                            "2) Maori months of the year\n"
                            "3) Maori days of the week\n"
                            "PLease enter a number between 1 and 3 to select quiz: ", int, [1, 2, 3])
+    # set question list from questions file depending on what quiz the user selected
     if quiz == 1:
         questions = MAORI_NUMBERS
     elif quiz == 2:
         questions = MAORI_MONTHS
     else:
         questions = MAORI_DAYS
+    # ask first question
     ask_question(questions)
