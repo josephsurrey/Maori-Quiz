a32ef69ef2cf69f3213c1e08f9a2cf6cbdf22f1a
josephsurrey <josephsurrey2018@gmail.com>
2023-05-10 11:05:50 +1200

    corrected creation date and added comments

--- check_answer.py
+++ check_answer.py
@@ -1,10 +1,12 @@
-""" Written by Joseph Surrey 05/05/2023
+""" Written by Joseph Surrey 08/05/2023
 """
 
 
 def check_answer(questions, answer, user_answer):
+    # if the users answer is the same as the answer then update the score
     if answer == user_answer:
         update_score(questions, "correct")
+    # if the users answer is not the same as the answer then update the score
     else:
         update_score(questions, "incorrect")
 
