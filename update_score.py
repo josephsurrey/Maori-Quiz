b674d39441a3696b1a5352df1604e5835cd259fc
josephsurrey <josephsurrey2018@gmail.com>
2023-05-10 20:00:14 +1200

    added comments to update_score

--- update_score.py
+++ update_score.py
@@ -5,6 +5,7 @@ from provide_feedback import provide_feedback
 
 
 def update_score(questions, result, score):
+    # if user gets question correct, increase score by 1
     if result == "correct":
         score += 1
     provide_feedback(questions, result, score)
